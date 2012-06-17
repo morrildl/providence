7804603b61eaf186e6f17f45f5ad1bb2a9dcc1b3
Dan Morrill <morrildl@setentio.us>
2012-06-16 17:30:28 -0700

    Adds ajar-detection and finishes time exclusion windows. Also a bit of refactoring.

--- providence/go/monitor.go
+++ providence/go/monitor.go
@@ -9,17 +9,40 @@ import (
   "time"
 )
 
+type sensorType int
+const (
+  window = iota
+  door
+  motion
+)
+
 type eventType int
 const (
   trip = iota
   reset
+  ajar
+  anomaly
 )
+
 type event struct {
   Which string
+  Type sensorType
   Action eventType
 }
 
-func ttyreader(c chan event) {
+type eventWindow struct {
+  hour int
+  minute int
+  duration time.Duration
+  dows []time.Weekday
+}
+
+/* Reads a USB TTY looking for JSON messages from a hardware monitor and
+injects low-level (trip and reset) eventTypes into the outgoing channel. Never
+reads from 'incoming'; accordingly, should never be registered for any message
+types or it will eventually deadlock when the channel buffer fills.
+ */
+func ttyreader(incoming chan event, outgoing chan event) {
   file, err := os.Open("/dev/ttyUSB0")
   if err != nil {
     fmt.Println("Error opening /dev/ttyUSB0, aborting")
@@ -31,11 +54,15 @@ func ttyreader(c chan event) {
   var event event
   for {
     dec.Decode(&event)
-    c <- event
+    fmt.Println("ttyreader sending", event.Which, event.Action)
+    outgoing <- event
   }
 }
 
-func recorder(events chan event) {
+/* Logs all events it gets to a sqlite3 database. Should be registered for all
+ * eventTypes. Never sends anything to the outgoing channel.
+ */
+func recorder(incoming chan event, outgoing chan event) {
   db, err := sql.Open("sqlite3", "./events.sqlite3")
   if err != nil {
     fmt.Println(err)
@@ -47,7 +74,7 @@ func recorder(events chan event) {
   }
   defer insert.Close()
   for {
-    event := <-events
+    event := <-incoming
     insert.Exec(event.Which, event.Action)
     if err != nil {
       fmt.Println(err)
@@ -55,78 +82,125 @@ func recorder(events chan event) {
   }
 }
 
-type window struct {
-  hour int
-  minute int
-  duration time.Duration
-  dows []time.Weekday
+type tripRecord struct {
+  when time.Time
+  next_send time.Duration
 }
-func decidererer(events chan event) {
-  /*
-   * Things it should do:
-   * - Declare a set of time windows
-   * - If door activity occurs outside that time window, flag
-   * - If activity occurs "a lot" regardless of time window, flag
-   * - If door looks like it's open, flag
-   */
+
+/* Looks for low-level events on the incoming channel and applies some
+ * heuristics to determine whether they are noteworthy. Will inject
+ * higher-level eventTypes (ajar, anomalous) to the outgoing channel as
+ * appropriate.
+ *
+ * Currently the heuristics are exclusion intervals and 'door is ajar'
+ * detection. Should only be registered for low-level events.
+ */
+func monitor(incoming chan event, outgoing chan event) {
   workdays := []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Thursday, time.Friday}
   weekends := []time.Weekday{time.Saturday, time.Sunday}
-  grr := func(s string) time.Duration {
-    d, _ := time.ParseDuration(s)
-    return d
+  windows := []eventWindow{
+    eventWindow{7, 30, 2 * time.Hour + 30 * time.Minute, workdays[:]},
+    eventWindow{5, 30, 4 * time.Hour + 30 * time.Minute, workdays[:]},
+    eventWindow{9,  0, 1 * time.Hour, weekends[:]}, // TODO: remove
+    //eventWindow{9,  0, 11 * time.Hour, weekends[:]},
   }
-  windows := []window{
-    window{7, 30, grr("2h30m"), workdays[:]},
-    window{5, 30, grr("4h30m"), workdays[:]},
-    window{9,  0, grr("11h0m"), weekends[:]},
-    window{0,  0, grr("2h0m"), workdays[:]},
-  }
-  //state := map[string]int{"FRONT_DOOR": 1, "GARAGE_DOOR": 1, "MOTION": 1}
+  // TODO ajar_threshold := 5 * time.Minute
+  // TODO resend_frequency := 10 * time.Minute
+  ajar_threshold := 5 * time.Second
+  resend_frequency := 1 * time.Minute
+  last_trips := make(map[string]*tripRecord)
+  ticker := time.Tick(1 * time.Second)
   for {
-    e := <-events
-    now := time.Now()
-    for _, w := range windows {
-      legit := false
-      for _, dow := range w.dows {
-        if now.Weekday() == dow {
-          legit = true
-          break
+    select {
+    case e := <-incoming:
+      now := time.Now()
+
+      // timestamp trips for ajar-detection, and clear on resets
+      if e.Action == trip {
+        last_trips[e.Which] = &tripRecord{now, ajar_threshold}
+      } else if e.Action == reset {
+        delete(last_trips, e.Which)
+      }
+
+      // check trips against exclusion intervals for anomalous events
+      if e.Action == trip {
+        in_window := false
+        for _, w := range windows {
+          legit := false
+          for _, dow := range w.dows {
+            if now.Weekday() == dow {
+              legit = true
+              break
+            }
+          }
+          if !legit {
+            continue
+          }
+          start := time.Date(now.Year(), now.Month(), now.Day(), w.hour, w.minute, 0, 0, time.Local)
+          end := start.Add(w.duration)
+          if now.After(start) && now.Before(end) {
+            in_window = true
+            break
+          }
+        }
+        if !in_window {
+          outgoing <- event{e.Which, 0, anomaly}
         }
       }
-      if !legit {
-        continue
-      }
-      start := time.Date(now.Year(), now.Month(), now.Day(), w.hour, w.minute, 0, 0, time.Local)
-      end := start.Add(w.duration)
-      if now.After(start) && now.Before(end) {
-        fmt.Println("Door event ", e.Which, "/", e.Action, " falls within window ", w.hour, w.minute)
+
+    case <- ticker:
+      // once per second, check whether anything is (still) Ajar & (re)transmit if it's time to
+      for which, last := range last_trips {
+        if time.Since(last.when) > ajar_threshold && time.Since(last.when) > last.next_send {
+          last.next_send += resend_frequency
+          outgoing <- event{which, 0, ajar}
+        }
       }
     }
   }
 }
 
+/* Looks for higher-level event types and escalates them for
+ * human review. Should only be registered for ajar and anomalous.
+ */
+func escalator(incoming chan event, outgoing chan event) {
+  for {
+    select {
+    case <- incoming:
+    }
+  }
+}
+
+/* Stores handler function and its state and registration info. */
 type handler struct {
-  f func(chan event)
+  f func(chan event, chan event)
   ch chan event
+  eventTypes map[eventType]int
 }
 
 func main() {
-  tty := make(chan event, 10)
-  go ttyreader(tty)
+  events := make(chan event, 10)
 
-  funcs := [](func(chan event)){recorder, decidererer}
-  handlers := make([]handler, len(funcs), len(funcs))
-  for i, f := range funcs {
-    handlers[i] = handler{f, make(chan event, 10)}
-    go f(handlers[i].ch)
+  handlers := []handler{
+    handler{ttyreader, make(chan event, 10), make(map[eventType]int)}, // no registrations
+    handler{recorder, make(chan event, 10), map[eventType]int{trip: 1, reset: 1, ajar: 1, anomaly: 1}},
+    handler{monitor, make(chan event, 10), map[eventType]int{trip: 1, reset: 1}},
+    handler{escalator, make(chan event, 10), map[eventType]int{ajar: 1, anomaly: 1}},
+  }
+  for _, h := range handlers {
+    go h.f(h.ch, events)
   }
 
   for {
-    evt := <-tty
+    evt := <-events
     fmt.Print(evt.Which + " ")
-    fmt.Println(map[eventType]string{trip: "Tripped", reset: "Reset"}[evt.Action])
-    for _, h := range handlers {
-      h.ch <- evt
+    fmt.Println(map[eventType]string{trip: "Tripped", reset: "Reset", ajar: "Ajar", anomaly: "Anomaly"}[evt.Action])
+    for i, h := range handlers {
+      _, ok := h.eventTypes[evt.Action]
+      if ok {
+        fmt.Println("Sending ", evt.Action, evt.Which, "to", i)
+        h.ch <- evt
+      }
     }
   }
 }
