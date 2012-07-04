7690da521d53942e5a9802a474c4db7ea8724395
Dan Morrill <morrildl@setentio.us>
2012-07-03 17:00:15 -0700

    Basic GCM round-trip now works.

---
+++ Lifer/src/dude/morrildl/lifer/GCMIntentService.java
@@ -0,0 +1,47 @@
+package dude.morrildl.lifer;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gcm.GCMBaseIntentService;
+
+public class GCMIntentService extends GCMBaseIntentService {
+	public static final String SENDER_ID = "25235963451";
+
+	public GCMIntentService() {
+		super(SENDER_ID);
+	}
+
+	@Override
+	protected void onError(Context context, String message) {
+		Log.e("IntentService", message);
+	}
+
+	@Override
+	protected void onMessage(Context context, Intent intent) {
+		Intent i = new Intent(context, LiferActivity.class);
+		i.putExtras(intent.getExtras());
+		PendingIntent pi = PendingIntent.getActivity(context, 42, i, 0);
+		Notification n = (new Notification.Builder(context))
+				.setContentTitle("Event fired").setContentIntent(pi)
+				.setSmallIcon(android.R.drawable.ic_delete).setAutoCancel(true)
+				.getNotification();
+		((NotificationManager) context
+				.getSystemService(Context.NOTIFICATION_SERVICE)).notify(42, n);
+	}
+
+	@Override
+	protected void onRegistered(Context context, String regId) {
+		context.getSharedPreferences("main", 0).edit()
+				.putString("regid", regId).commit();
+	}
+
+	@Override
+	protected void onUnregistered(Context context, String regId) {
+		context.getSharedPreferences("main", 0).edit().remove("regid").commit();
+	}
+}
--- Lifer/src/dude/morrildl/lifer/LiferActivity.java
+++ Lifer/src/dude/morrildl/lifer/LiferActivity.java
@@ -2,12 +2,44 @@ package dude.morrildl.lifer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gcm.GCMRegistrar;
 
 public class LiferActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		GCMRegistrar.checkDevice(this);
+		GCMRegistrar.checkManifest(this);
+		String regId = GCMRegistrar.getRegistrationId(this);
+		if (regId == null || "".equals(regId)) {
+			GCMRegistrar.register(this, GCMIntentService.SENDER_ID);
+		}
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		String regId = GCMRegistrar.getRegistrationId(this);
+		if (regId != null && !"".equals(regId)) {
+			((TextView) findViewById(R.id.reg_id)).setText(regId);
+			Log.e("booga", regId);
+		} else {
+			((TextView) findViewById(R.id.reg_id)).setText("no reg ID");			
+		}
+		
+		Bundle b = getIntent().getExtras();
+		if (b != null) {
+			StringBuilder sb = new StringBuilder();
+			for (String k : b.keySet()) {
+				sb.append(k).append(" = ").append(b.getString(k)).append("\n");
+			}
+			((TextView)findViewById(R.id.main_text)).setText(sb.toString());
+		}
+	}
 }
