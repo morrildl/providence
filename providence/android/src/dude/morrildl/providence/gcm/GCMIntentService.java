8fafafe2520fffe043e9d9d1ac6fed1f717e103d
Dan Morrill <morrildl@setentio.us>
2012-08-12 17:11:38 -0700

    Dialing back notifications for motion sensor trips. Since motion sensors trip a lot, getting a notification each time is spammy. Now we only notify on motion sensor trips if it's been > 4 hours since the last one. This also adds handling for out of order GCM messages and makes the database GC more defensive by having the helper do it itself whenever a writable database is requested.

--- providence/android/src/dude/morrildl/providence/gcm/GCMIntentService.java
+++ providence/android/src/dude/morrildl/providence/gcm/GCMIntentService.java
@@ -1,11 +1,16 @@
 package dude.morrildl.providence.gcm;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
@@ -16,6 +21,9 @@ import dude.morrildl.providence.R;
 import dude.morrildl.providence.panopticon.OpenHelper;
 
 public class GCMIntentService extends GCMBaseIntentService {
+	// 4 hours
+	private static final long MOTION_NOTIFICATION_THRESHOLD = 4 * 60 * 60 * 1000;
+
 	public static final String SENDER_ID = "25235963451";
 
 	public GCMIntentService() {
@@ -29,27 +37,79 @@ public class GCMIntentService extends GCMBaseIntentService {
 
 	@Override
 	protected void onMessage(Context context, Intent intent) {
+		boolean isMotion = (Integer.parseInt(intent.getStringExtra("SensorType")) == 2);
+		boolean notifyOnMotion = false;
+		boolean skipMotionUpdate = false;
+
+		String which = intent.getStringExtra("WhichName");
+		String ts = intent.getStringExtra("When");
+
 		OpenHelper helper = new OpenHelper(context);
 		SQLiteDatabase db = helper.getWritableDatabase();
-		db.beginTransaction();
+
+		// insert all incoming events into the DB, even motion events
 		ContentValues values = new ContentValues();
-		values.put("which", intent.getStringExtra("WhichName"));
+		values.put("which", which);
 		values.put("type", intent.getStringExtra("SensorTypeName"));
 		values.put("event", intent.getStringExtra("EventName"));
-		values.put("ts", intent.getStringExtra("When"));
+		values.put("ts", ts);
+		db.beginTransaction();
 		db.insert("events", null, values);
-		db.execSQL(OpenHelper.GC);
+
+		// filter out motion events, unless it's been a long time since the last
+		if (isMotion) {
+			Cursor c = db.query("last_motion", new String[] { "ts" },
+					"which = ?", new String[] { which }, null, null, null);
+			if (c.moveToFirst()) { // i.e. we have heard from this sensor before
+				Calendar lastMotion = Calendar.getInstance();
+				Calendar currentMotion = Calendar.getInstance();
+				try {
+					SimpleDateFormat sdf = new SimpleDateFormat(
+							"yyyy'-'MM'-'dd'T'HH:mm:ss");
+					lastMotion.setTime(sdf.parse(c.getString(0)));
+					currentMotion.setTime(sdf.parse(ts));
+
+					if (currentMotion.after(lastMotion)) {
+						// only notify if it's been > threshold since last one
+						// Note that this applies ONLY to firing a notification;
+						// it's up to other UI to show or not show motion events
+						// as is appropriate in context
+						notifyOnMotion = ((currentMotion.getTimeInMillis() - lastMotion
+								.getTimeInMillis()) > MOTION_NOTIFICATION_THRESHOLD);
+					} else {
+						// it's possible to receive these message out of order;
+						// tell ourselves to keep the more recent message
+						skipMotionUpdate = true;
+					}
+				} catch (ParseException e) {
+					Log.w("GCM onMessage", "malformed date");
+				}
+			}
+			// update or insert the timestamp, unless message was out of order
+			if (!skipMotionUpdate) {
+				db.execSQL(OpenHelper.REPLACE_LAST_MOTION, new Object[] {
+						which, ts });
+			}
+		}
+
 		db.setTransactionSuccessful();
 		db.endTransaction();
-		
-		Intent i = new Intent(context, PanopticonActivity.class);
-		PendingIntent pi = PendingIntent.getActivity(context, 42, i, 0);
-		Notification n = (new Notification.Builder(context))
-				.setContentTitle("Event fired").setContentIntent(pi)
-				.setSmallIcon(R.drawable.ic_stat_event).setAutoCancel(true)
-				.getNotification();
-		((NotificationManager) context
-				.getSystemService(Context.NOTIFICATION_SERVICE)).notify(42, n);
+		db.close();
+
+		if (!isMotion || notifyOnMotion) {
+			Intent i = new Intent(context, PanopticonActivity.class);
+			PendingIntent pi = PendingIntent.getActivity(context, 42, i, 0);
+			Notification n = (new Notification.Builder(context))
+					.setContentTitle(
+							intent.getStringExtra("WhichName") + " "
+									+ intent.getStringExtra("EventName"))
+					.setContentIntent(pi)
+					.setSmallIcon(R.drawable.ic_stat_event).setAutoCancel(true)
+					.getNotification();
+			((NotificationManager) context
+					.getSystemService(Context.NOTIFICATION_SERVICE)).notify(42,
+					n);
+		}
 	}
 
 	@Override
--- providence/android/src/dude/morrildl/providence/panopticon/OpenHelper.java
+++ providence/android/src/dude/morrildl/providence/panopticon/OpenHelper.java
@@ -5,21 +5,38 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class OpenHelper extends SQLiteOpenHelper {
+	public static final String CREATE_EVENTS = "create table events (id integer primary key, which text not null, type text not null, event text not null, ts timestamp)";
+	public static final String CREATE_LAST_MOTION = "create table last_motion (which text not null primary key, ts timestamp)";
+	public static final int DATABASE_VERSION = 2;
+	public static final String REPLACE_LAST_MOTION = "replace into last_motion values (?, ?)";
+
 	private static final String DATABASE_NAME = "providence";
-	public static final int DATABASE_VERSION = 1;
-	public static final String GC = "delete from events where ts < datetime('now', '-1week')";
-	public static final String CREATE = "create table events (id integer auto_increment, which text not null, type text not null, event text not null, ts timestamp)";
-	
+	private static final String GC = "delete from events where ts < datetime('now', '-1week')";
+
 	public OpenHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 
 	@Override
+	public synchronized SQLiteDatabase getWritableDatabase() {
+		SQLiteDatabase db = super.getWritableDatabase();
+		// run our housekeeping query (to prevent unbounded DB size)
+		// before we hand over the connection to the caller
+		db.execSQL(OpenHelper.GC);
+		return db;
+	}
+
+	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL(CREATE);
+		db.execSQL(CREATE_EVENTS);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		if (oldVersion < 2) {
+			db.execSQL("drop table events");
+			db.execSQL(CREATE_EVENTS);
+			db.execSQL(CREATE_LAST_MOTION);
+		}
 	}
 }
